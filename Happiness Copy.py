7c5f034de1d8122f356337cb3fd9ab0ee3b83085
xxiaoling <xz3490@nyu.edu>
2022-03-10 16:15:08 -0500

    added 5 other colors values (needs to be tested)

--- Happiness Copy.py
+++ Happiness Copy.py
@@ -40,26 +40,51 @@ while(True):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     #Red Color Detection:
-    red_lower = np.array([160,70,50])
+    red_lower = np.array([160, 70, 50])
     red_upper = np.array([180, 255, 255])
     red_mask = cv2.inRange(hsv, red_lower, red_upper)
     result_red = cv2.bitwise_and(frame, frame, mask=red_mask)
+
+    #Orange Color Detection:
+    orange_lower = np.array([5, 50, 50])
+    orange_upper = np.array([15, 255, 255])
+    orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
+    result_orange = cv2.bitwise_and(frame, frame, mask=orange_mask)
+
+    #Yellow Color Detection:
+    yellow_lower = np.array([55, 50, 70])
+    yellow_upper = np.array([35, 255, 255])
+    yellow_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+    yellow_orange = cv2.bitwise_and(frame, frame, mask=yellow_mask)
 
     #Green Color Detection:   
     # INSERT CODE: Define green lower boundary
     green_lower = np.array([20, 40, 60])
     # INSERT CODE: Define green upper boundary
-    green_upper = np.array([102, 255, 255])
+    green_upper = np.array([100, 255, 255])
     # INSERT CODE: Create green mask
     green_mask = cv2.inRange(hsv, green_lower, green_upper)
     # INSERT CODE: Use bitwise_and to isolate green color from the video
     result_green = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+    #Blue Color Detection:
+    blue_lower = np.array([90, 50, 70])
+    blue_upper = np.array([130, 255, 255])
+    blue_mask = cv2.inRange(hsv, blue_lower, blue_upper)
+    result_blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+
+    #Purple Color Detection:
+    purple_lower = np.array([130, 50, 70])
+    purple_upper = np.array([160, 255, 255])
+    purple_mask = cv2.inRange(hsv, purple_lower, purple_upper)
+    result_purple = cv2.bitwise_and(frame, frame, mask=purple_mask)
+
     #Result:
     # INSERT CODE: Combine the results so both red and green colors are isolated from the video using bitwise_or
     result_red_and_green = cv2.bitwise_or(result_red, result_green)
 
     # INSERT CODE: Display the final result
-    cv2.imshow("Red and Green Detection in Real-Time", result_red_and_green)
+    cv2.imshow("Color Detection in Real-Time", result_red_and_green)
 
     rawCapture.truncate(0) #Stop rawCapture to clear the stream in preparation for the next frame
   
