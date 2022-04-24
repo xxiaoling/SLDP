992813879aef622ff26256f0a995158bfd4aedca
sunnnnyli <100388296+sunnnnyli@users.noreply.github.com>
2022-04-24 18:57:48 -0400

    updated code

--- Happiness.py
+++ Happiness.py
@@ -30,7 +30,7 @@ def detect_color(color, match):
 	
 	print(color, "detected")
 	pwm.start(5)
-	engine.say(color + "detected")
+	engine.say("huh" + color + "detected")
 	time.sleep(3)
 	print(color, "matches with", match)
 	engine.say(color + " matches with " + match)
@@ -68,8 +68,8 @@ while (True):
 		# Orange Color Detection:
 		# orange_lower = np.array([200, 190, 1])
 		# orange_upper = np.array([255, 255, 18])
-		orange_lower = np.array([10, 50, 20])
-		orange_upper = np.array([23, 255, 255])
+		orange_lower = np.array([10, 70, 50])
+		orange_upper = np.array([23, 179, 179])
 		orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
 		result_orange = cv2.bitwise_and(frame, frame, mask = orange_mask)
 
@@ -104,21 +104,40 @@ while (True):
 		# Magenta Color Detection:
 		# magenta_lower = np.array([130, 50, 70])
 		# magenta_upper = np.array([160, 255, 255])
-		magenta_lower = np.array([140, 50, 75])
+		magenta_lower = np.array([140, 156, 156])
 		magenta_upper = np.array([155, 255, 255])
 		magenta_mask = cv2.inRange(hsv, magenta_lower, magenta_upper)
 		result_magenta = cv2.bitwise_and(frame, frame, mask = magenta_mask)
 
+		# Pink Color Detection:
+		pink_lower = np.array([140, 10, 10])
+		pink_upper = np.array([180, 105, 255])
+		pink_mask = cv2.inRange(hsv, pink_lower, pink_upper)
+		result_pink = cv2.bitwise_and(frame, frame, mask=pink_mask)
+		
+		# Brown Color Detection:
+		brown_lower = np.array([10, 180, 180])
+		brown_upper = np.array([42, 255, 255])
+		brown_mask = cv2.inRange(hsv, brown_lower, brown_upper)
+		result_brown = cv2.bitwise_and(frame, frame, mask=brown_mask)
+		
+		# Gray Color Detection:
+		gray_lower = np.array([120, 120, 120])
+		gray_upper = np.array([255, 255, 255])
+		gray_mask = cv2.inRange(hsv, gray_lower, gray_upper)
+		result_gray = cv2.bitwise_and(frame, frame, mask=gray_mask)
+
 		# Result:
 		# Combine the results so all colors are isolated from the video using bitwise_or
-		result_red_green = cv2.bitwise_or(result_red, result_green)
-		result_blue_orange = cv2.bitwise_or(result_blue, result_orange)
-		result_yellow_magenta = cv2.bitwise_or(result_yellow, result_magenta)
-
-		result_some_colors = cv2.bitwise_or(result_red_green, result_blue_orange)
-		result_more_colors = cv2.bitwise_or(result_some_colors, result_cyan)
-
-		result_colors = cv2.bitwise_or(result_more_colors, result_yellow_magenta)
+		result_red_and_green = cv2.bitwise_or(result_red, result_green)
+		result_blue_and_orange = cv2.bitwise_or(result_blue, result_orange)
+		result_yellow_and_magenta = cv2.bitwise_or(result_yellow, result_magenta)
+		result_cyan_and_pink = cv2.bitwise_or(result_cyan, result_pink)
+		result_brown_and_gray = cv2.bitwise_or(result_brown, result_gray)
+		result_some_colors = cv2.bitwise_or(result_red_and_green, result_blue_and_orange)
+		result_more_colors = cv2.bitwise_or(result_some_colors, result_cyan_and_pink)
+		result_even_more_colors = cv2.bitwise_or(result_more_colors, result_brown_and_gray)
+		result_colors = cv2.bitwise_or(result_more_colors, result_yellow_and_magenta)
 
 		# Display the final result
 		cv2.imshow("Color Detection in Real-Time", result_colors)
@@ -137,41 +156,58 @@ while (True):
 			blue = cv2.countNonZero(blue_mask)
 			magenta = cv2.countNonZero(magenta_mask)
 			cyan = cv2.countNonZero(cyan_mask)
+			pink = cv2.countNonZero(pink_mask)
+			brown = cv2.countNonZero(brown_mask)
+			gray = cv2.countNonZero(gray_mask)
+
+			percent = 0.08
 
 			# RED
-			if (red > green and red > orange and red > yellow and red > blue and red > magenta and red > cyan):
+			if (red > percent * pixels) and (red > green and red > orange and red > yellow and red > blue and red > magenta and red > cyan and red > pink and red > brown and red > gray):
 				detect_color("Red", "Orange")
 
 			# GREEN
-			elif (green > red and green > orange and green > yellow and green > blue and green > magenta and green > cyan):
+			elif (green > percent * pixels) and (green > red and green > orange and green > yellow and green > blue and green > magenta and green > cyan and green > pink and green > brown and green > gray):
 				detect_color("Green", "Orange")
 
 			# ORANGE
-			elif (orange > red and orange > green and orange > yellow and orange > blue and orange > magenta and orange > cyan):
+			elif (orange > percent * pixels) and (orange > red and orange > green and orange > yellow and orange > blue and orange > magenta and orange > cyan and orange > pink and orange > brown and orange > gray):
 				detect_color("Orange", "Red")
 
 			# YELLOW
-			elif (yellow > red and yellow > green and yellow > orange and yellow > blue and yellow > magenta and yellow > cyan):
+			elif (yellow > percent * pixels) and (yellow > red and yellow > green and yellow > orange and yellow > blue and yellow > magenta and yellow > cyan and yellow > pink and yellow > brown and yellow > gray):
 				detect_color("Yellow", "Orange")
 
 			# BLUE
-			elif (blue > red and blue > green and blue > orange and blue > yellow and blue > magenta and blue > cyan):
+			elif (blue > percent * pixels) and (blue > red and blue > green and blue > orange and blue > yellow and blue > magenta and blue > cyan and blue > pink and blue > brown and blue > gray):
 				detect_color("Blue", "Orange")
 
 			# MAGENTA
-			elif (magenta > red and magenta > green and magenta > orange and magenta > yellow and magenta > blue and magenta > cyan):
+			elif (magenta > percent * pixels) and (magenta > red and magenta > green and magenta > orange and magenta > yellow and magenta > blue and magenta > cyan and magenta > pink and magenta > brown and magenta > gray):
 				detect_color("Magenta", "Orange")
 
 			# CYAN
-			elif (cyan > red and cyan > green and cyan > orange and cyan > yellow and cyan > blue and cyan > magenta):
+			elif (cyan > percent * pixels) and (cyan > red and cyan > green and cyan > orange and cyan > yellow and cyan > blue and cyan > magenta and cyan > pink and cyan > brown and cyan > gray):
 				detect_color("Cyan", "Orange")
+
+			# PINK
+			elif (pink > percent * pixels) and (pink > red and pink > green and pink > orange and pink > yellow and pink > blue and pink > magenta and pink > cyan and pink > brown and pink > gray):
+				detect_color("Pink", "Orange")
+
+			# BROWN
+			elif (brown > percent * pixels) and (brown > red and brown > green and brown > orange and brown > yellow and brown > blue and brown > magenta and brown > cyan and brown > pink and brown > gray):
+				detect_color("Brown", "Orange")
+
+			# GRAY
+			elif (gray > percent * pixels) and (gray > red and gray > green and gray > orange and gray > yellow and gray > blue and gray > magenta and gray > cyan and gray > pink and gray > brown):
+				detect_color("Gray", "Orange")
 
 			# NO COLOR DETECTED
 			else:
 				print("Color not detected")
 				# Stop servo rotation
 				pwm.stop()
-				engine.say("Color not detected")
+				engine.say("huh Color not detected")
 				engine.runAndWait()
 
 			print("------------------------")
